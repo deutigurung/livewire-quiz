0e340a1eaf1fe9c6d7ad6a19e0bb08ada1f1b385
Deuti Gurung <deuti@ininepal.com>
2023-05-11 21:25:38 +0545

    quiz listed for public and registered user in home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $query = Quiz::withCount('questions')->has('questions')
+                ->when(auth()->guest() || !auth()->user()->is_admin,function($q){
+                    return $q->where('published',true);
+                })
+                ->orderBy('id')->get();
+        $public = $query->where('public',true);
+        $registered = $query->where('public',false);
+
+        return view('home',compact('public', 'registered'));
     }
 }
